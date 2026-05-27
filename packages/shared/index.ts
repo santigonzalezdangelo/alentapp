0d4741ac3426cd9f30ebbcd92cdf8814f7f48dd6
leannoval <leandronoval4@gmail.com>
2026-05-27 09:58:52 -0300

    fix(shared): agregar tipo UpdateSportRequest eliminado

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -183,3 +183,10 @@ export interface CreateSportRequest {
   additional_price: number;
   requires_medical_certificate: boolean;
 }
+
+export interface UpdateSportRequest {
+  description?: string;
+  max_capacity?: number;
+  additional_price?: number;
+  requires_medical_certificate?: boolean;
+}
